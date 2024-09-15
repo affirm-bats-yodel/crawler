aea6126d1340dbba6c749e78579649c5e59a2171
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-09-15 10:06:32 +0000

    feat(pkg/request/request.go): add Response struct
      * returning a only a []byte and error is not
      suitable to handle after calling Get.
    
      * defined a new Response struct and replace
      []byte to Response struct to handle future
      request.

--- pkg/request/request.go
+++ pkg/request/request.go
@@ -1,13 +1,33 @@
 package request
 
-import "context"
+import (
+	"context"
+	"io"
+	"net/http"
+)
 
 // Request an interface for Handle Request
 type Request interface {
 	// Get request to get Page
-	//
-	// return a HTML Content
-	Get(ctx context.Context, url string) ([]byte, error)
+	Get(ctx context.Context, url string) (*Response, error)
 	// Shutdown Gracefully Shutdown Externally managed resources
 	Shutdown(ctx context.Context) error
+}
+
+type Response struct {
+	// StatusCode HTTP Status Code
+	StatusCode int
+	// ContentLength Size of the Content
+	ContentLength int64
+	// Header Response Header
+	Header *http.Header
+	// Cookies List of the Cookies
+	Cookies []*http.Cookie
+	// Body a Body
+	Body io.ReadCloser
+}
+
+// GetContentType Get "Content-Type" from Header
+func (r *Response) GetContentType() string {
+	return r.Header.Get("Content-Type")
 }
