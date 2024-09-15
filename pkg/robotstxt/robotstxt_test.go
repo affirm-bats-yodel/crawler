f2e8da5810a6b4c9cc7720d686adaa1af1e865ca
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-09-15 08:15:01 +0000

    chore(pkg/robotstxt): touch robotstxt_test.go

---
+++ pkg/robotstxt/robotstxt_test.go
@@ -0,0 +1,12 @@
+package robotstxt_test
+
+import (
+	"testing"
+
+	"github.com/affirm-bats-yodel/crawler/pkg/robotstxt"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestParser Test "robots.txt" Parser
+func TestParser(t *testing.T) {
+}
