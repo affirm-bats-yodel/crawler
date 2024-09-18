8edf06277c2ce84b3712a138d906cc651384ffb5
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-09-18 06:14:36 +0000

    test(keys_test.go): fix assertion type

--- pkg/indexer/badgerindexer/keys_test.go
+++ pkg/indexer/badgerindexer/keys_test.go
@@ -9,22 +9,25 @@ import (
 
 func TestKeyObjectToURI(t *testing.T) {
 	for _, tc := range []struct {
+		name     string
 		objHash  string
 		optIdx   []uint64
-		expected string
+		expected []byte
 	}{
 		{
+			name:     "KeyForReferenceCounts",
 			objHash:  "xxx",
 			optIdx:   nil,
-			expected: "/xxx/references",
+			expected: []byte("/xxx/references"),
 		},
 		{
+			name:     "KeyForReferenceIndex",
 			objHash:  "xxx",
 			optIdx:   []uint64{1},
-			expected: "/xxx/references/1",
+			expected: []byte("/xxx/references/1"),
 		},
 	} {
-		t.Run(tc.expected, func(t *testing.T) {
+		t.Run(tc.name, func(t *testing.T) {
 			act := badgerindexer.KeyObjectToURI(tc.objHash, tc.optIdx...)
 			assert.Equal(t, tc.expected, act)
 		})
