2a069375985383ba501284e7f2d1effc3a562324
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-09-18 05:53:45 +0000

    feat(pkg/indexer/error.go): add error for invalid uri

--- pkg/indexer/error.go
+++ pkg/indexer/error.go
@@ -9,3 +9,11 @@ func IsErrInvalidObjectHash(e error) bool {
 
 // ErrInvalidObjHash Error for when given object hash is invalid
 var ErrInvalidObjHash = errors.New("error: invalid object hash")
+
+// IsErrInvalidURI Check error is ErrInvalidURI
+func IsErrInvalidURI(e error) bool {
+	return errors.Is(e, ErrInvalidURI)
+}
+
+// ErrInvalidURI Error for when given uri is invalid
+var ErrInvalidURI = errors.New("error: invalid uri")
