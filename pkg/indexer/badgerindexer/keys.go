5efa1e1809438d580f9b3a110770390a54c7e184
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-09-18 06:25:34 +0000

    feat(keys.go): add uri to hash mapper

--- pkg/indexer/badgerindexer/keys.go
+++ pkg/indexer/badgerindexer/keys.go
@@ -1,6 +1,7 @@
 package badgerindexer
 
 import (
+	"encoding/base64"
 	"path/filepath"
 	"strconv"
 )
@@ -11,5 +12,14 @@ func KeyObjectToURI(objHash string, optIdx ...uint64) []byte {
 	if optIdx != nil {
 		optIdxVal = strconv.FormatUint(optIdx[0], 10)
 	}
-	return []byte(filepath.Join("/", objHash, "/references", optIdxVal))
+	return []byte(filepath.Join("/objects/", objHash, "/references", optIdxVal))
+}
+
+// KeyURIToObject Generate a Key that references uri to objects
+func KeyURIToObject(uri string, isTotal bool) []byte {
+	var totalPath string
+	if isTotal {
+		totalPath = "/total"
+	}
+	return []byte(filepath.Join("/uris/", base64.StdEncoding.EncodeToString([]byte(uri)), totalPath))
 }
