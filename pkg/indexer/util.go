74ebbc5e0d51017e1b0699636dab165ab00cd905
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-09-18 05:52:14 +0000

    feat(pkg/indexer/util.go): add uri validator

--- pkg/indexer/util.go
+++ pkg/indexer/util.go
@@ -1,8 +1,33 @@
 package indexer
 
+import "net/url"
+
 // IsObjHashValid Check objHash is valid
 //
 // It just checking objHash length is 64 Bytes
 func IsObjHashValid(objHash string) bool {
 	return len(objHash) == 64
+}
+
+// IsValidURI Check uri is valid
+//
+// - try to parse given uri
+//
+// - check parsed uri's scheme is starting
+// with http:// or https://
+func IsValidURI(uri string) bool {
+	u, err := url.Parse(uri)
+	if err != nil {
+		return false
+	}
+	// check scheme is valid
+	//
+	// only accepts starting with
+	// http:// or https://
+	switch u.Scheme {
+	case "http", "https":
+		return true
+	default:
+		return false
+	}
 }
