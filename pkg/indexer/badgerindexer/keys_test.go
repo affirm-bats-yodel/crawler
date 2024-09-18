85fbcabd8bf7ac919d38e72c58fe329237427039
affirm-bats-yodel <affirm-bats-yodel@duck.com>
2024-09-18 06:31:08 +0000

    test(keys_test.go): add key mapper test

--- pkg/indexer/badgerindexer/keys_test.go
+++ pkg/indexer/badgerindexer/keys_test.go
@@ -18,17 +18,44 @@ func TestKeyObjectToURI(t *testing.T) {
 			name:     "KeyForReferenceCounts",
 			objHash:  "xxx",
 			optIdx:   nil,
-			expected: []byte("/xxx/references"),
+			expected: []byte("/objects/xxx/references"),
 		},
 		{
 			name:     "KeyForReferenceIndex",
 			objHash:  "xxx",
 			optIdx:   []uint64{1},
-			expected: []byte("/xxx/references/1"),
+			expected: []byte("/objects/xxx/references/1"),
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			act := badgerindexer.KeyObjectToURI(tc.objHash, tc.optIdx...)
+			assert.Equal(t, tc.expected, act)
+		})
+	}
+}
+
+func TestURIToObject(t *testing.T) {
+	for _, tc := range []struct {
+		name     string
+		uri      string
+		isTotal  bool
+		expected []byte
+	}{
+		{
+			name:     "KeyForTotalObjectCounts",
+			uri:      "https://example.com",
+			isTotal:  true,
+			expected: []byte("/uris/aHR0cHM6Ly9leGFtcGxlLmNvbQ==/total"),
+		},
+		{
+			name:     "KeyForObjectReferences",
+			uri:      "https://example.com",
+			isTotal:  false,
+			expected: []byte("/uris/aHR0cHM6Ly9leGFtcGxlLmNvbQ=="),
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			act := badgerindexer.KeyURIToObject(tc.uri, tc.isTotal)
 			assert.Equal(t, tc.expected, act)
 		})
 	}
